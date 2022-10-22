70a3e7209b41266c4f2e87fdf3a6ff75ac0e78c3
Piotr Błaszyk <piopio555888@gmail.com>
2022-10-22 14:14:35 +0100

    [psb120, dc1020, avb320] (refactor, fix) Fixed race condition and improved style

--- src/hash_set_sequential.h
+++ src/hash_set_sequential.h
@@ -1,84 +1,79 @@
 #ifndef HASH_SET_SEQUENTIAL_H
 #define HASH_SET_SEQUENTIAL_H
 
+#include <algorithm>
 #include <cassert>
 #include <vector>
-#include <algorithm>
 
 #include "src/hash_set_base.h"
 
-template<typename T>
-class HashSetSequential : public HashSetBase<T> {
+template <typename T> class HashSetSequential : public HashSetBase<T> {
 public:
-    explicit HashSetSequential(size_t capacity) : bucket_count_(capacity), elem_count_(0) {
-        table_.resize(bucket_count_);
-        for (size_t i = 0; i < bucket_count_; i++) {
-            table_[i] = std::vector<T>();
-        }
+  explicit HashSetSequential(size_t capacity)
+      : bucket_count_(capacity), elem_count_(0) {
+    table_.resize(bucket_count_);
+    for (size_t i = 0; i < bucket_count_; i++) {
+      table_[i] = std::vector<T>();
     }
+  }
 
-    bool Add(T elem) final {
-        if (ContainsNoLock(elem)) return false;
-        size_t my_bucket = std::hash<T>()(elem) % bucket_count_;
-        table_[my_bucket].push_back(elem);
-        elem_count_++;
-        if (Policy()) {
-            Resize();
-        }
-        return true;
+  bool Add(T elem) final {
+    if (ContainsNoLock(elem))
+      return false;
+    size_t my_bucket = std::hash<T>()(elem) % bucket_count_;
+    table_[my_bucket].push_back(elem);
+    elem_count_++;
+    if (Policy()) {
+      Resize();
     }
+    return true;
+  }
 
-    bool Remove(T elem) final {
-        if (!ContainsNoLock(elem)) return false;
-        assert(elem_count_ != 0);
-        size_t my_bucket = std::hash<T>()(elem) % bucket_count_;
-        table_[my_bucket].erase(std::find(table_[my_bucket].begin(), table_[my_bucket].end(), elem));
-        elem_count_--;
-        return true;
-    }
+  bool Remove(T elem) final {
+    if (!ContainsNoLock(elem))
+      return false;
+    assert(elem_count_ != 0);
+    size_t my_bucket = std::hash<T>()(elem) % bucket_count_;
+    table_[my_bucket].erase(
+        std::find(table_[my_bucket].begin(), table_[my_bucket].end(), elem));
+    elem_count_--;
+    return true;
+  }
 
-    [[nodiscard]] bool Contains(T elem) final {
-        return ContainsNoLock(elem);
-    }
+  [[nodiscard]] bool Contains(T elem) final { return ContainsNoLock(elem); }
 
-    [[nodiscard]] size_t Size() const final {
-        return elem_count_;
-    }
-
+  [[nodiscard]] size_t Size() const final { return elem_count_; }
 
 private:
+  std::vector<std::vector<T>> table_;
+  size_t bucket_count_;
+  size_t elem_count_;
 
-    std::vector<std::vector<T>> table_;
-    size_t bucket_count_;
-    size_t elem_count_;
+  bool Policy() { return (elem_count_ / bucket_count_) > 4; }
 
-    bool Policy() {
-        return (elem_count_ / bucket_count_) > 4;
+  void Resize() {
+    size_t old_capacity = bucket_count_;
+    size_t new_capacity = 2 * old_capacity;
+    bucket_count_ = new_capacity;
+    std::vector<std::vector<T>> table;
+    table.resize(new_capacity);
+    for (size_t i = 0; i < new_capacity; i++) {
+      table[i] = std::vector<T>();
     }
-
-    void Resize() {
-        size_t old_capacity = bucket_count_;
-        size_t new_capacity = 2 * old_capacity;
-        bucket_count_ = new_capacity;
-        std::vector<std::vector<T>> table;
-        table.resize(new_capacity);
-        for (size_t i = 0; i < new_capacity; i++) {
-            table[i] = std::vector<T>();
-        }
-        for (std::vector<T> bucket : table_) {
-            for (T elem : bucket) {
-                table[std::hash<T>()(elem) % new_capacity].push_back(elem);
-            }
-        }
-        table_ = table;
+    for (std::vector<T> bucket : table_) {
+      for (T elem : bucket) {
+        table[std::hash<T>()(elem) % new_capacity].push_back(elem);
+      }
     }
+    table_ = table;
+  }
 
-    bool ContainsNoLock(T elem) {
-        size_t my_bucket = std::__1::hash<T>()(elem) % bucket_count_;
-        std::__1::vector<T> small_table = table_[my_bucket];
-        return std::find(small_table.begin(), small_table.end(), elem) != small_table.end();
-    }
-
+  bool ContainsNoLock(T elem) {
+    size_t my_bucket = std::hash<T>()(elem) % bucket_count_;
+    std::vector<T> small_table = table_[my_bucket];
+    return std::find(small_table.begin(), small_table.end(), elem) !=
+           small_table.end();
+  }
 };
 
-#endif  // HASH_SET_SEQUENTIAL_H
+#endif // HASH_SET_SEQUENTIAL_H
--- src/hash_set_striped.h
+++ src/hash_set_striped.h
@@ -1,106 +1,103 @@
 #ifndef HASH_SET_STRIPED_H
 #define HASH_SET_STRIPED_H
 
-#include <cassert>
-#include <vector>
 #include <algorithm>
-#include <mutex>
 #include <atomic>
+#include <cassert>
 #include <deque>
+#include <mutex>
+#include <vector>
 
 #include "src/hash_set_base.h"
 
-template <typename T>
-class HashSetStriped : public HashSetBase<T> {
+template <typename T> class HashSetStriped : public HashSetBase<T> {
 public:
-    explicit HashSetStriped(size_t capacity) : bucket_count_(capacity), initial_bucket_count_(capacity), elem_count_(0) {
-        table_.resize(bucket_count_);
-        mutexes_ = std::vector<std::mutex>(initial_bucket_count_);
-        for (size_t i = 0; i < bucket_count_; i++) {
-            table_[i] = std::vector<T>();
-        }
-
+  explicit HashSetStriped(size_t capacity)
+      : bucket_count_(capacity), initial_bucket_count_(capacity),
+        elem_count_(0) {
+    table_.resize(bucket_count_.load());
+    mutexes_ = std::vector<std::mutex>(initial_bucket_count_);
+    for (size_t i = 0; i < bucket_count_.load(); i++) {
+      table_[i] = std::vector<T>();
     }
+  }
 
-    bool Add(T elem) final {
-        size_t my_lock = std::hash<T>()(elem) % initial_bucket_count_;
-        mutexes_[my_lock].lock();
-        size_t my_bucket = std::hash<T>()(elem) % bucket_count_;
-        if (ContainsNoLock(elem, my_bucket)) {
-            mutexes_[my_lock].unlock();
-            return false;
-        }
-        table_[my_bucket].push_back(elem);
-        elem_count_.fetch_add(1);
-        mutexes_[my_lock].unlock();
-        if (Policy()) {
-            for (size_t i = 0; i < mutexes_.size(); i ++) {
-                mutexes_[i].lock();
-            }
-            Resize();
-            for(size_t i = 0; i < mutexes_.size(); i ++) {
-                mutexes_[i].unlock();
-            }
-        }
-        return true;
+  bool Add(T elem) final {
+    size_t my_lock = std::hash<T>()(elem) % initial_bucket_count_;
+    mutexes_[my_lock].lock();
+    size_t my_bucket = std::hash<T>()(elem) % bucket_count_.load();
+    if (ContainsNoLock(elem, my_bucket)) {
+      mutexes_[my_lock].unlock();
+      return false;
     }
+    table_[my_bucket].push_back(elem);
+    elem_count_.fetch_add(1);
+    mutexes_[my_lock].unlock();
+    if (Policy()) {
+      for (auto &mutex : mutexes_) {
+        mutex.lock();
+      }
+      Resize();
+      for (auto &mutex : mutexes_) {
+        mutex.unlock();
+      }
+    }
+    return true;
+  }
 
-    bool Remove(T elem) final {
-        size_t my_lock = std::hash<T>()(elem) % initial_bucket_count_;
-        std::scoped_lock<std::mutex> lock(mutexes_[my_lock]);
-        size_t my_bucket = std::hash<T>()(elem) % bucket_count_;
-        if (!ContainsNoLock(elem, my_bucket)) return false;
-        assert(elem_count_ != 0);
-        table_[my_bucket].erase(std::find(table_[my_bucket].begin(), table_[my_bucket].end(), elem));
-        elem_count_.fetch_sub(1);
-        return true;
-    }
+  bool Remove(T elem) final {
+    size_t my_lock = std::hash<T>()(elem) % initial_bucket_count_;
+    std::scoped_lock<std::mutex> lock(mutexes_[my_lock]);
+    size_t my_bucket = std::hash<T>()(elem) % bucket_count_.load();
+    if (!ContainsNoLock(elem, my_bucket))
+      return false;
+    assert(elem_count_ != 0);
+    table_[my_bucket].erase(
+        std::find(table_[my_bucket].begin(), table_[my_bucket].end(), elem));
+    elem_count_.fetch_sub(1);
+    return true;
+  }
 
-    [[nodiscard]] bool Contains(T elem) final {
-        size_t my_lock = std::hash<T>()(elem) % initial_bucket_count_;
-        std::scoped_lock<std::mutex> lock(mutexes_[my_lock]);
-        size_t my_bucket = std::hash<T>()(elem) % bucket_count_;
-        return ContainsNoLock(elem, my_bucket);
-    }
+  [[nodiscard]] bool Contains(T elem) final {
+    size_t my_lock = std::hash<T>()(elem) % initial_bucket_count_;
+    std::scoped_lock<std::mutex> lock(mutexes_[my_lock]);
+    size_t my_bucket = std::hash<T>()(elem) % bucket_count_.load();
+    return ContainsNoLock(elem, my_bucket);
+  }
 
-    [[nodiscard]] size_t Size() const final {
-        return elem_count_.load();
-    }
+  [[nodiscard]] size_t Size() const final { return elem_count_.load(); }
 
 private:
+  std::vector<std::vector<T>> table_;
+  std::atomic<size_t> bucket_count_;
+  size_t initial_bucket_count_;
+  std::atomic<size_t> elem_count_;
+  std::vector<std::mutex> mutexes_;
 
-    std::vector<std::vector<T>> table_;
-    size_t bucket_count_;
-    size_t initial_bucket_count_;
-    std::atomic<size_t> elem_count_;
-    std::vector<std::mutex> mutexes_;
-    std::mutex resize_mutex_;
+  bool ContainsNoLock(T elem, size_t my_bucket) {
+    std::vector<T> small_table = table_[my_bucket];
+    return std::find(small_table.begin(), small_table.end(), elem) !=
+           small_table.end();
+  }
 
-    bool ContainsNoLock(T elem, size_t my_bucket) {
-        std::__1::vector<T> small_table = table_[my_bucket];
-        return std::find(small_table.begin(), small_table.end(), elem) != small_table.end();
+  bool Policy() { return (elem_count_.load() / bucket_count_.load()) > 4; }
+
+  void Resize() {
+    size_t old_capacity = bucket_count_.load();
+    size_t new_capacity = 2 * old_capacity;
+    bucket_count_.store(new_capacity);
+    std::vector<std::vector<T>> table;
+    table.resize(new_capacity);
+    for (size_t i = 0; i < new_capacity; i++) {
+      table[i] = std::vector<T>();
     }
-
-    bool Policy() {
-        return (elem_count_.load() / bucket_count_) > 4;
+    for (std::vector<T> bucket : table_) {
+      for (T elem : bucket) {
+        table[std::hash<T>()(elem) % new_capacity].push_back(elem);
+      }
     }
-
-    void Resize() {
-        size_t old_capacity = bucket_count_;
-        size_t new_capacity = 2 * old_capacity;
-        bucket_count_ = new_capacity;
-        std::vector<std::vector<T>> table;
-        table.resize(new_capacity);
-        for (size_t i = 0; i < new_capacity; i++) {
-            table[i] = std::vector<T>();
-        }
-        for (std::vector<T> bucket : table_) {
-            for (T elem : bucket) {
-                table[std::hash<T>()(elem) % new_capacity].push_back(elem);
-            }
-        }
-        table_ = table;
-    }
+    table_ = table;
+  }
 };
 
-#endif  // HASH_SET_STRIPED_H
+#endif // HASH_SET_STRIPED_H
